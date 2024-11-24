140ab6bb506d1a1d5188171ea2a1fff1e963328c
RakibR7 <G00408644@ATU.IE>
2024-11-24 23:08:26 +0000

    first commit

---
+++ src/main/java/atu/ie/libraryadvisor/LibraryAdvisorApplication.java
@@ -0,0 +1,13 @@
+package atu.ie.libraryadvisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAdvisorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryAdvisorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/atu/ie/libraryadvisor/LibraryAdvisorApplicationTests.java
@@ -0,0 +1,13 @@
+package atu.ie.libraryadvisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryAdvisorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
